e5a1d8a1b5cc628b44c32d447341157801aaffbd
Brian Hodge <brian.hodgebh@googlemail.com>
2026-03-09 16:36:17 +0000

    Moved header layout to it's own component, added new component to main, projects and solutions pages

--- src/app/projects/[id]/page.tsx
+++ src/app/projects/[id]/page.tsx
@@ -1,11 +1,20 @@
 import ProjectDetail from '../../../components/ProjectDetail';
+import Header from '../../../components/Header'
 
 async function ProjectPage({ params }: { params: { id: string } }) {
   const { id } = await params; // In newer Next.js versions, params is an async object
 
   return (
-    <div>
-      <ProjectDetail projectId={id} />
+    <div className="min-h-screen bg-[#F9F6F4] p-4 text-slate-700 selection:bg-bran-pink/30 md:p-8 lg:p-12 font-sans">
+      <div className="fixed inset-0 overflow-hidden pointer-events-none">
+        <div className="absolute -left-[10%] top-[20%] h-[500px] w-[500px] rounded-full bg-brand-slate/10 blur-[120px]" />
+        <div className="absolute -right-[10%] top-[10%] h-[500px] w-[500px] rounded-full bg-bran-pink/10 blur-[120px]" />
+      </div>
+
+      <div className="mx-auto max-w-7xl relative">
+        <Header />
+        <ProjectDetail projectId={id} />
+      </div>
     </div>
   );
 }
